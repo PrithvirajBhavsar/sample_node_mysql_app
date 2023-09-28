572711e6008d19acec659634150aed5d9a4eb1c2
Prithviraj Bhavsar <prithvirajbhavsar@gmail.com>
2023-09-28 21:42:07 +0530

    init

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "13.235.103.223",
+  host: "ec2-13-235-103-223.ap-south-1.compute.amazonaws.com",
   user: "prithvi",
   password: "password",
   database: "customersdb",
